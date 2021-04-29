563dae6a531c7cda2ba143740ad10d63453ef966
Artem Oshepkov <artemoshepkov@users.noreply.github.com>
2021-04-29 11:59:12 +0700

    Clang files and fix yml

--- src/libchessviz/lib.cpp
+++ src/libchessviz/lib.cpp
@@ -1,13 +1,14 @@
-#include <libchessviz/lib.h>
 #include <iostream>
+#include <libchessviz/lib.h>
 using namespace std;
 
 struct coord {
-  char type, x1, x2, sign;
-  int y1, y2;
+    char type, x1, x2, sign;
+    int y1, y2;
 };
 
-int TransX(char x) {
+int TransX(char x)
+{
     switch (x) {
     case 'a':
         return 1;
@@ -29,7 +30,8 @@ int TransX(char x) {
         return 0;
     }
 }
-int TransY(int y) {
+int TransY(int y)
+{
     switch (y) {
     case 1:
         y = 7;
@@ -61,12 +63,14 @@ int TransY(int y) {
     return y;
 }
 
-void dash() {
+void dash()
+{
     for (int i = 0; i < 20; ++i)
         cout << "_";
     cout << endl;
 }
-void out_board(char board[9][9]) {
+void out_board(char board[9][9])
+{
     dash();
     for (int i = 0; i < 9; ++i) {
         for (int j = 0; j < 9; ++j)
@@ -76,23 +80,31 @@ void out_board(char board[9][9]) {
     dash();
 }
 
-void Motion(char board[9][9], coord str) {
-    board[TransY(str.y2)][TransX(str.x2)] = board[TransY(str.y1)][TransX(str.x1)];
+void Motion(char board[9][9], coord str)
+{
+    board[TransY(str.y2)][TransX(str.x2)]
+            = board[TransY(str.y1)][TransX(str.x1)];
     board[TransY(str.y1)][TransX(str.x1)] = ' ';
 }
 
-int CheckRangeX(coord first, coord second) {
-    if (TransX(first.x1) != 0 && TransX(first.x2) != 0 && TransX(second.x1) != 0 && TransX(second.x2) != 0)
+int CheckRangeX(coord first, coord second)
+{
+    if (TransX(first.x1) != 0 && TransX(first.x2) != 0 && TransX(second.x1) != 0
+        && TransX(second.x2) != 0)
         return 1;
     return 0;
 }
-int CheckRangeY(coord first, coord second) {
-    if (TransY(first.y1) != -1 && TransY(first.y2) != -1 && TransY(second.y1) != -1 && TransY(second.y2) != -1)
+int CheckRangeY(coord first, coord second)
+{
+    if (TransY(first.y1) != -1 && TransY(first.y2) != -1
+        && TransY(second.y1) != -1 && TransY(second.y2) != -1)
         return 1;
     return 0;
 }
-int CheckType (char board[9][9], coord first, coord second) {
-  if (first.type == board[TransY(first.y1)][TransX(first.x1)] && second.type == board[TransY(second.y1)][TransX(second.x1)])
-    return 1;
-  return 0;
+int CheckType(char board[9][9], coord first, coord second)
+{
+    if (first.type == board[TransY(first.y1)][TransX(first.x1)]
+        && second.type == board[TransY(second.y1)][TransX(second.x1)])
+        return 1;
+    return 0;
 }
